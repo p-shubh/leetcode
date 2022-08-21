69a4fe893323d375ca8c5442905038eb42da8757
shubham prajapati <shubhamprajapati032@gmail.com>
2022-08-21 13:53:48 +0530

    missing number completed

--- missing_number_268/main.go
+++ missing_number_268/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 
-	a := []int{3, 0, 1}
+	a := []int{1,2}
 
 	fmt.Println(missingNumber(a))
 
@@ -15,33 +16,39 @@ func main() {
 func missingNumber(nums []int) int {
 
 	length := len(nums)
-	fmt.Println("length :", length)
-	var i int
 
-	numsL := length - 1
+	sort.Ints(nums)
 
-	for i := 0; i <= length; i++ {
+	var result int
 
-		mid := length / 2
+	fmt.Println("shorted :", nums)
 
-		if mid > i {
-			// fmt.Println("mid :", mid, "i:", i)
-			for j := 0; j < mid; j++ {
-				if nums[numsL] != i {
-					return i
-				}
+	for c, d := range nums {
+
+		if length == 1 {
+			if d > 0 {
+				result = nums[c] - 1
+				return result
+			} else {
+				result = nums[c] + 1
+				return result
 			}
 
-		} else if mid < i {
-			for j := mid; j < numsL; j++ {
-				if nums[numsL] != i {
-					return i
-				}
+		} else if c <= len(nums) {
+
+			fmt.Println("c :", c, "d:", d)
+			if c != nums[c] {
+				nums[c] = c
+				result = nums[c]
+				break
+
+			} else if nums[length-1] != length {
+				result = nums[c] + 1
 			}
 		}
 
 	}
 
-	return i
+	return result
 
 }
